2c46522d909e26dce3216249a278708a435fedd2
Samuel VIVIER <samvivier25@gmail.com>
2024-03-08 15:53:38 +0100

    Add contact

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup>
 import About from '../components/About.vue';
+import Contact from "../components/Contact.vue";
 </script>
 
 <template>
   <main>
     <About></About>
-    
+    <Contact></Contact>
   </main>
 </template>
